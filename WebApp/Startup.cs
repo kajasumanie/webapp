2145a0fd1f6d03fbd6fc6d9c183e30c87316b3a0
kajasumanie <kkajasu@gmail.com>
2019-10-08 11:22:33 +0530

    Update Startup.cs

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -44,6 +44,7 @@ namespace WebApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                //kajas jenkins testing
             });
         }
     }
